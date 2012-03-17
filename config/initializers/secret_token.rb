8537e4a43f11e9b175067c953192d6a42b1736bb
Collin Schaafsma <collin@quickleft.com>
2012-03-17 16:42:21 -0600

    initial rails build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dieter::Application.config.secret_token = '24cf8fa73ad82ce93f0fe2e0c6d32f77a4526eb2abf2c0497a7fcdbaed3cd9f9d9b3c71d2fc580162e1f36f9143ccbe726b97480a04e4673febf22b863055cea'
